afadc4f26076e002a4015d2df9f9478e09954431
lelutmaonk <putuadiputra51@gmail.com>
2023-07-07 21:04:52 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+    <div className="flex justify-center bg-blue-600 min-h-screen items-center">
+      Hello World
+    </div>
+    </>
+  )
+}
+
+export default App
